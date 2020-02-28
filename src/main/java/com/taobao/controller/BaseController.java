cd51e01fb7597c52cf76f81fda30b974f8513a25
Disda <497457669@qq.com>
2020-02-28 16:16:34 +0800

    1

--- src/main/java/com/taobao/controller/BaseController.java
+++ src/main/java/com/taobao/controller/BaseController.java
@@ -18,22 +18,22 @@ public class BaseController {
 
     //定义excpetionhandler解决未被controller层吸收的exception
     //加上ResponseBody来达到效果
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public Object handlerException(HttpServletRequest request, Exception ex){
-        Map<String,Object> responseData=new HashMap();
-        if(ex instanceof BusinessException){
-            BusinessException businessException=(BusinessException)ex;
-            responseData.put("errCode", businessException.getErrCode());
-            responseData.put("errMsg",businessException.getErrMsg());
-        }else{
-            ex.printStackTrace();
-            responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
-            responseData.put("errMsg", EmBusinessError.UNKNOWN_ERROR.getErrMsg());
-        }
-
-        return CommonReturnType.create(responseData,"fail");
-
-    }
+//    @ExceptionHandler(Exception.class)
+//    @ResponseStatus(HttpStatus.OK)
+//    @ResponseBody
+//    public Object handlerException(HttpServletRequest request, Exception ex){
+//        Map<String,Object> responseData=new HashMap();
+//        if(ex instanceof BusinessException){
+//            BusinessException businessException=(BusinessException)ex;
+//            responseData.put("errCode", businessException.getErrCode());
+//            responseData.put("errMsg",businessException.getErrMsg());
+//        }else{
+//            ex.printStackTrace();
+//            responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
+//            responseData.put("errMsg", EmBusinessError.UNKNOWN_ERROR.getErrMsg());
+//        }
+//
+//        return CommonReturnType.create(responseData,"fail");
+//
+//    }
 }

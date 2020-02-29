e88d8d36decdcdd45a9ce90e92323ff7623249eb
Your Name <497457669@qq.com>
2020-02-29 18:24:00 +0800

    1

--- src/main/java/com/taobao/config/RedisConfig.java
+++ src/main/java/com/taobao/config/RedisConfig.java
@@ -1,5 +1,10 @@
 package com.taobao.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.taobao.serializer.JodaDateTimeJsonDeserializer;
+import com.taobao.serializer.JodaDateTimeJsonSerializer;
+import org.joda.time.DateTime;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -20,6 +25,17 @@ public class RedisConfig {
         redisTemplate.setKeySerializer(stringRedisSerializer);
         //解决value的序列化方式
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer=new Jackson2JsonRedisSerializer(Object.class);
+        //将自定义的序列化方式注册进去
+        ObjectMapper objectMapper=new ObjectMapper();
+        SimpleModule simpleModule=new SimpleModule();
+        simpleModule.addSerializer(DateTime.class,new JodaDateTimeJsonSerializer());
+        simpleModule.addDeserializer(DateTime.class,new JodaDateTimeJsonDeserializer());
+
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        objectMapper.registerModule(simpleModule);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+
         redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
         return redisTemplate;
     }

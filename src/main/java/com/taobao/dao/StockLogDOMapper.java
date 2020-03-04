df37d13cba58aa8334a09f1c347e487ab6f594eb
Your Name <497457669@qq.com>
2020-03-04 12:54:24 +0800

    haha

---
+++ src/main/java/com/taobao/dao/StockLogDOMapper.java
@@ -0,0 +1,17 @@
+package com.taobao.dao;
+
+import com.taobao.dataobject.StockLogDO;
+
+public interface StockLogDOMapper {
+    int deleteByPrimaryKey(String stockLogId);
+
+    int insert(StockLogDO record);
+
+    int insertSelective(StockLogDO record);
+
+    StockLogDO selectByPrimaryKey(String stockLogId);
+
+    int updateByPrimaryKeySelective(StockLogDO record);
+
+    int updateByPrimaryKey(StockLogDO record);
+}
